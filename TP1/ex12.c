263d678d5c792bb49d503fc972e271b6c9b5cde3
negorigin <corentin.frery@gmail.com>
2016-11-18 15:05:48 +0000

    Exercice 13
    Exercice 12 divisé sur 2 processus

--- TP1/ex12.c
+++ TP1/ex12.c
@@ -10,10 +10,20 @@ int main() {
 	for(i=0; i<10000; i++) {
 	    tab[i]=(rand()%1000)+1;
 	}
+	
 	printf("Cases contenant 42 :\n");
-	for(i=0; i<10000; i++) {
-	    if(tab[i]==42)
-	        printf("%d\n", i);
+	
+	if(fork()==0) {
+		for(i=5000; i<10000; i++) {
+    	    if(tab[i]==42)
+    	        printf("%d\n", i);
+    	}
+	} else {
+		for(i=0; i<5000; i++) {
+    	    if(tab[i]==42)
+    	        printf("%d\n", i);
+    	}
+    	wait();
 	}
     
     return 0;
