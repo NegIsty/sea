9afa3d6f21c226ee78d70be2b2819a55f962a31e
negorigin <corentin.frery@gmail.com>
2016-11-18 15:47:31 +0000

    Exercice 14
    Exercice 12 sur n processus

--- TP1/ex12.c
+++ TP1/ex12.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
     int tab[10000];
-	int i;
+	int i, j, proc;
+	
+	if(argc>1)
+		proc=atoi(argv[1]);
+	else {
+		printf("Nombre de processus : ");
+		if(scanf("%d", &proc)!=1) {
+			printf("Le nombre de processus doit être un entier\n");
+			return 1;
+		}
+	}
 	
 	srand(time(NULL));
 	
@@ -13,18 +23,23 @@ int main() {
 	
 	printf("Cases contenant 42 :\n");
 	
-	if(fork()==0) {
-		for(i=5000; i<10000; i++) {
-    	    if(tab[i]==42)
-    	        printf("%d\n", i);
-    	}
-	} else {
-		for(i=0; i<5000; i++) {
-    	    if(tab[i]==42)
-    	        printf("%d\n", i);
-    	}
-    	wait();
+	for(j=1; j<proc; j++) {
+		if(fork()==0) {
+			for(i=(j-1)*10000/proc; i<j*10000/proc; i++) {
+	    	    if(tab[i]==42)
+	    	        printf("%d\n", i);
+	    	}
+	    	return 0;
+		}
 	}
+	
+	for(i=(proc-1)*10000/proc; i<10000; i++) {
+	    if(tab[i]==42)
+	        printf("%d\n", i);
+	}
+	
+	for(j=0; j<proc-1; j++)
+		wait();
     
     return 0;
 }
