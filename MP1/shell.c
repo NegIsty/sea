78391aaf524ee3d49cbdb893f64ab4c240437c44
negorigin <corentin.frery@gmail.com>
2016-11-21 12:11:12 +0000

    [MP1] 02. Un shell simple
    Decomposition de la saisie utilisateur

--- MP1/shell.c
+++ MP1/shell.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+static void search(char *chaine);
 
 int main() {
 	char commande[200];
+	char *ligne, *token, *fin;
 	
 	while(1) {
 		printf("%% ");
@@ -11,9 +16,27 @@ int main() {
 			printf("\n");
 			return 0;
 		} else {
-			printf("%s", commande);
+			search(commande);
+			ligne=strdup(commande);
+			fin=ligne;
+			
+			while(token=strsep(&fin, " ")) {
+				printf("%s\n", token);
+			}
+			
+			free(ligne);
 		}
 	}
 	
 	return 0;
+}
+
+/*Elimine le saut de ligne du fgets
+Source : https://openclassrooms.com/courses/realiser-des-saisies-securisees-grace-a-fgets*/
+static void search(char *chaine) {
+    char *p=strchr(chaine, '\n');
+
+    if (p) {
+        *p = 0;
+    }
 }
