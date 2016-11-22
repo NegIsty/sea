6c15ec669b30138a27fe9861d8354f97885c7b8c
negorigin <corentin.frery@gmail.com>
2016-11-22 01:07:25 +0000

    [MP1] 05. Fonctionnalites avancees
    Implementation des redirections d'entrees sorties standard

--- MP1/shell.c
+++ MP1/shell.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 
 static void search(char *chaine);
 
 int main() {
-	char commande[200];
-	char path[200];
+	char commande[1000];
+	char path[1000];
 	char *ligne, *fin, *tmp;
 	char *token[20];
-	int fils, status, i;
+	char *nomFichier;
+	int fils, status, i, direction, fichier;
+	int stdoutCopy=dup(1), stdinCopy=dup(0);
 	
 	while(1) {
 		/*Affiche le répertoire courant*/
@@ -48,18 +51,83 @@ int main() {
 			/*Autre commande*/
 			else {
 				i=1;
+				direction=0;
 				
 				/*Sépare le reste de la commande en tokens*/
 				while(tmp=strsep(&fin, " ")) {
+					
+					/*Test s'il y a une redirection*/
+					if(!(strcmp(tmp, ">"))) {
+						direction=1;
+						break;
+					} else if(!(strcmp(tmp, "<"))) {
+						direction=2;
+						break;
+					} else if(!(strcmp(tmp, "2>"))) {
+						direction=3;
+						break;
+					}
+					
 					token[i++]=tmp;
 				}
+				
 				token[i]=NULL;
 				
-				/*Crée un fils qui exécute la commande*/
-				if((fils=fork())==0) {
-					execvp(token[0], token);	
+				/*Gestion de la redirection*/
+				if(direction) {
+					nomFichier=strsep(&fin, " ");
+					
+					switch(direction) {
+						case 1:
+							fichier=open(nomFichier, O_CREAT | O_WRONLY, S_IRWXU | S_IRGRP | S_IROTH);
+							break;
+						case 2:
+							fichier=open(nomFichier, O_RDONLY);
+							break;
+						case 3:
+							fichier=open(nomFichier, O_CREAT | O_WRONLY | O_APPEND, S_IRWXU | S_IRGRP | S_IROTH);
+							break;
+					}
+					
+					if(fichier<0) {
+						printf("Erreur lors de l'ouverture du fichier\n");
+					} else {
+						switch(direction) {
+							case 1:
+								dup2(fichier, 1);
+								break;
+							case 2:
+//								dup2(fichier, 0);
+								token[i++]=nomFichier;
+								token[i]=NULL;
+								break;
+							case 3:
+								dup2(fichier, 1);
+								break;
+						}
+						
+						/*Crée un fils qui exécute la commande*/
+						if((fils=fork())==0) {
+							execvp(token[0], token);	
+						}
+						
+						waitpid(fils, &status, WUNTRACED | WCONTINUED);
+						
+						dup2(stdoutCopy, 1);
+//						dup2(stdinCopy, 0);
+						
+						close(fichier);
+					}
 				}
-				waitpid(fils, &status, WUNTRACED | WCONTINUED);
+				
+				else {
+					/*Crée un fils qui exécute la commande*/
+					if((fils=fork())==0) {
+						execvp(token[0], token);	
+					}
+					
+					waitpid(fils, &status, WUNTRACED | WCONTINUED);
+				}
 			}
 			
 			free(ligne);
