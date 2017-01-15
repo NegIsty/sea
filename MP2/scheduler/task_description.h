c4392a74c202b91823c7175a6451b49f0bea00cf
Corentin Frery <corentin.frery@gmail.com>
2017-01-15 18:28:34 +0100

    [MP2] Ordonnancement Temps-Réel (20)
    Test des systèmes de tâches du cours
    Tous les résultats sont identiques, mis à part celui de "Effet Domino" pour RM

--- MP2/scheduler/task_description.h
+++ MP2/scheduler/task_description.h
@@ -7,10 +7,52 @@ tproc tasks[] = {
     {5,8,2,2}
 };*/
 
-int max_time = 40;
+/*int max_time = 40;
 tproc tasks[] = {
      //pid  //activation  //length  //remaining  //period
     {1,     0,            1,        1,           3       },
     {2,     0,            1,        1,           4       },
     {3,     0,            2,        2,           6       },
+};*/
+
+
+/* Question 20 */
+
+/* Exemple RM 1 */
+/*int max_time = 20;
+tproc tasks[] = {
+     //pid  //activation  //length  //remaining  //period
+    {1,     0,            1,        1,           2       },
+    {2,     0,            3,        3,           10      },
+};*/
+/* Même résultat */
+
+/* Exemple RM 2 */
+/*int max_time = 20;
+tproc tasks[] = {
+     //pid  //activation  //length  //remaining  //period
+    {1,     0,            1,        1,           2       },
+    {2,     0,            5,        5,           10      },
+};*/
+/* Même résultat */
+
+/* Exemple EDF 1 */
+/*int max_time = 40;
+tproc tasks[] = {
+     //pid  //activation  //length  //remaining  //period
+    {1,     0,            1,        1,           4       },
+    {2,     0,            2,        2,           6       },
+    {3,     0,            3,        3,           8       },
+};*/
+/* Même résultat */
+
+/* Exemple Effet Domino */
+int max_time = 24;
+tproc tasks[] = {
+     //pid  //activation  //length  //remaining  //period
+    {1,     0,            2,        2,           5       },
+    {2,     0,            2,        2,           6       },
+    {3,     0,            2,        2,           7       },
+    {4,     0,            2,        2,           8       },
 };
+/* Même résultat pour EDF, mais pas pour RM */
